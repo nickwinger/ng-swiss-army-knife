1f0b0ff48b18eaec4d6d1629717a58fbd3994198
Nick Winger <WINGER@kapsch.net>
2019-05-07 11:12:29 +0200

    new version 1.0.14

--- projects/ng-swiss-army-knife/src/lib/helper/index.ts
+++ projects/ng-swiss-army-knife/src/lib/helper/index.ts
@@ -1,7 +1,9 @@
 import { FormHelper } from './form';
 import { MiscHelper } from './misc';
+import { FileHelper } from './file';
 
 export class Helper {
   static form = FormHelper;
   static misc = MiscHelper;
+  static file = FileHelper;
 }
