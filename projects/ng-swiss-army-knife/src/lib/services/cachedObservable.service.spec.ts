664de031ed4f06fd8606828cfbca53eb9999190f
Nick <nick@bit4bit.at>
2021-04-26 14:03:54 +0200

    new version 1.4.1

--- projects/ng-swiss-army-knife/src/lib/services/cachedObservable.service.spec.ts
+++ projects/ng-swiss-army-knife/src/lib/services/cachedObservable.service.spec.ts
@@ -1,7 +1,5 @@
 import { TestBed } from '@angular/core/testing';
 import { CachedObservableService } from './cachedObservable.service';
-import { BehaviorSubject, of } from 'rxjs';
-import { delay, take } from 'rxjs/operators';
 
 describe('CachedObservableService', () => {
   beforeEach(() => {
@@ -14,34 +12,4 @@ describe('CachedObservableService', () => {
     expect(service).toBeTruthy();
   });
 
-  it('should cache and the reset the cache', async () => {
-    const service: CachedObservableService = TestBed.get(CachedObservableService);
-    const sub = new BehaviorSubject(1);
-    const ob = sub.asObservable();
-
-    let v = await service.getOrCreate('test1', 2, ob)
-      .pipe(take(1)).toPromise();
-    expect(v).toBe(1);
-
-    sub.next(2);
-
-    v = await service.getOrCreate('test1', 2, ob).pipe(take(1)).toPromise();
-    expect(v).toBe(1);
-
-    // wait 2 seconds
-    await of('k').pipe(delay(2000), take(1)).toPromise();
-
-    v = await service.getOrCreate('test1', 2, ob).pipe(take(1)).toPromise();
-    expect(v).toBe(2);
-
-    sub.next(3);
-
-    v = await service.getOrCreate('test1', 2, ob).pipe(take(1)).toPromise();
-    expect(v).toBe(2);
-
-    service.reset('test1');
-
-    v = await service.getOrCreate('test1', 2, ob).pipe(take(1)).toPromise();
-    expect(v).toBe(3);
-  });
 });
